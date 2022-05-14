029f3c37838994b51a630b47a3e5ab47b1bb9981
iDiego5614 <diego5614yt@gmail.com>
2022-05-13 23:13:28 -0500

    refactor(backend): 🔧 Console.Log Deleted

--- backend/src/hooks/isFollow.ts
+++ backend/src/hooks/isFollow.ts
@@ -5,15 +5,8 @@ const isFollow = (userFollowing : IUser , userToFollow : IUser) =>
   const { followings } = userFollowing
   const { followers } = userToFollow
 
-  console.log({ followings , followers });
-  console.log(userToFollow._id);
-  
-  console.time("Followers")
-  
   const includesToFollow = followings.includes(userToFollow._id)
   const includesFollowing = followers.includes(userFollowing._id)
-
-  console.timeEnd("Followers")
 
   return includesToFollow && includesFollowing
 }
